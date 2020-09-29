c0189fe15a60e0f32cfb411b6b867f8954c0977e
Jihoon Song <jihoonsong.dev@gmail.com>
2020-09-29 11:34:46 +0000

    Add hash_table_handler.{c,h}
    Hash table handler handles hash tables.
    
    All handlers are invoked by mainloop.
    
    Resolves: #3

---
+++ src/lib/kernel/hash_table_handler.c
@@ -0,0 +1,4 @@
+#include "hash_table_handler.h"
+#include <assert.h>	// Instead of 	#include "../debug.h"
+
+#define ASSERT(CONDITION) assert(CONDITION)	// patched for proj0-2
---
+++ src/lib/kernel/hash_table_handler.h
@@ -0,0 +1,8 @@
+#ifndef __LIB_KERNEL_HASH_TABLE_HANDLER_H
+#define __LIB_KERNEL_HASH_TABLE_HANDLER_H
+
+/* Hash table handler.
+   // TODO: Complete document.
+*/
+
+#endif /* lib/kernel/hash_table_handler.h */
