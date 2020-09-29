438162bbe7d42122119d0a92914844a03381c095
Jihoon Song <jihoonsong.dev@gmail.com>
2020-09-29 11:34:38 +0000

    Add list_handler.{c,h}
    List handler handles doubly linked lists.
    
    All handlers are invoked by mainloop.
    
    Resolves: #3

---
+++ src/lib/kernel/list_handler.c
@@ -0,0 +1,4 @@
+#include "list_handler.h"
+#include <assert.h>	// Instead of 	#include "../debug.h"
+
+#define ASSERT(CONDITION) assert(CONDITION)	// patched for proj0-2
---
+++ src/lib/kernel/list_handler.h
@@ -0,0 +1,8 @@
+#ifndef __LIB_KERNEL_LIST_HANDLER_H
+#define __LIB_KERNEL_LIST_HANDLER_H
+
+/* List handler.
+   // TODO: Complete document.
+*/
+
+#endif /* lib/kernel/list_handler.h */
