9ecd4544f4f5a5b9d2b1e7216cbb0e592220378d
Jihoon Song <jihoonsong.dev@gmail.com>
2020-09-29 12:44:21 +0000

    Call mainloop in main function
    Resolves: #3

--- src/lib/kernel/main.c
+++ src/lib/kernel/main.c
@@ -1,5 +1,12 @@
+#include "mainloop.h"
+
+/* Initialize and launch mainloop. Call termination after mainloop is over. */
 int
 main (void)
 {
+  mainloop_initialize();
+  mainloop_launch();
+  mainloop_terminate();
+
   return 0;
 }
--- src/lib/kernel/mainloop.c
+++ src/lib/kernel/mainloop.c
@@ -2,3 +2,21 @@
 #include <assert.h>	// Instead of 	#include "../debug.h"
 
 #define ASSERT(CONDITION) assert(CONDITION)	// patched for proj0-2
+
+/* Call all initializers that need to be invoked before mainloop begins. */
+void
+mainloop_initialize (void)
+{
+}
+
+/* Release memory. */
+void
+mainloop_terminate (void)
+{
+}
+
+/* Start mainloop. */
+void
+mainloop_launch (void)
+{
+}
