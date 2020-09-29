5ba47db39760584ce9c57ebae80247e0b0acf906
Jihoon Song <jihoonsong.dev@gmail.com>
2020-09-29 15:30:37 +0000

    Receives input from STDIN
    The length of the input is 100, which is an arbitrary chosen large
    enough number.
    
    Resolves: #4

--- src/lib/kernel/mainloop.c
+++ src/lib/kernel/mainloop.c
@@ -1,7 +1,12 @@
 #include "mainloop.h"
 #include <assert.h>	// Instead of 	#include "../debug.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 #define ASSERT(CONDITION) assert(CONDITION)	// patched for proj0-2
+#define INPUT_LEN 80
+
+static bool receive_input (char *input);
 
 /* Call all initializers that need to be invoked before mainloop begins. */
 void
@@ -19,4 +24,23 @@ mainloop_terminate (void)
 void
 mainloop_launch (void)
 {
+  char input[INPUT_LEN] = {0,};
+
+  while (true)
+    {
+      if (receive_input (input))
+        {
+          printf ("%s", input);
+        }
+    }
+}
+
+/* Reads stream data from stdin and stores it in INPUT.
+   Returns true if read succeeds, false otherwise. */
+static bool
+receive_input (char *input)
+{
+  ASSERT (input != NULL);
+
+  return fgets (input, INPUT_LEN, stdin) != NULL;
 }
