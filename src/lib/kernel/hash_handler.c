1373a4fd6dd63fadee6e91091bdcec8be57da5be
Jihoon Song <jihoonsong.dev@gmail.com>
2020-09-30 19:34:42 +0000

    Add hash command table.
    Hash command table consists of tuples of hash command type, name and
    a function pointer to its execution function.
    
    Resolves: #6

--- src/lib/kernel/hash_handler.c
+++ src/lib/kernel/hash_handler.c
@@ -4,6 +4,19 @@
 
 #define ASSERT(CONDITION) assert(CONDITION)	// patched for proj0-2
 
+/* Hash command. */
+typedef enum hash_cmd_type {CREATE, DELETE, DUMPDATA, HASH_APPLY, HASH_CLEAR,
+                            HASH_DELETE, HASH_EMPTY, HASH_FIND, HASH_INSERT,
+                            HASH_REPLACE, HASH_SIZE,
+                            HASH_CMD_COUNT, NONE} hash_cmd_type;
+typedef void (*hash_cmd_ptr) (const int, const char *[]);
+struct hash_cmd_table
+  {
+    const hash_cmd_type type;
+    const char *name;
+    const hash_cmd_ptr execute;
+  };
+
 static hash_cmd_type convert_to_hash_cmd_type (const char *cmd);
 static void execute_create (const int argc, const char *argv[]);
 static void execute_delete (const int argc, const char *argv[]);
@@ -16,6 +29,20 @@ static void execute_hash_find (const int argc, const char *argv[]);
 static void execute_hash_insert (const int argc, const char *argv[]);
 static void execute_hash_replace (const int argc, const char *argv[]);
 static void execute_hash_size (const int argc, const char *argv[]);
+
+/* Hash command table. */
+static const struct hash_cmd_table hash_cmd_table[HASH_CMD_COUNT] = \
+  {{CREATE, "create", execute_create},
+   {DELETE, "delete", execute_delete},
+   {DUMPDATA, "dumpdata", execute_dumpdata},
+   {HASH_APPLY, "hash_apply", execute_hash_apply},
+   {HASH_CLEAR, "hash_clear", execute_hash_clear},
+   {HASH_DELETE, "hash_delete", execute_hash_delete},
+   {HASH_EMPTY, "hash_empty", execute_hash_empty},
+   {HASH_FIND, "hash_find", execute_hash_find},
+   {HASH_INSERT, "hash_insert", execute_hash_insert},
+   {HASH_REPLACE, "hash_replace", execute_hash_replace},
+   {HASH_SIZE, "hash_size", execute_hash_size}};
 
 /* TODO: Complete document. */
 void
