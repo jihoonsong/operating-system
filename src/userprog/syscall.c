e9b7e05d36cb23d825129ad09c056db54c8ad1a5
Jihoon Song <jihoonsong.dev@gmail.com>
2020-10-24 08:29:58 +0000

    Read or write one byte to validate pointers
    Read or write one byte to validate pointers to user virtual address
    before execute system handler.
    
    See [Pintos] 3.1.5 "Accessing User Memory".
    
    Resolves: #25

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,9 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+static int get_user (const uint8_t *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -17,4 +20,34 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a segfault occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  ASSERT (uaddr != NULL);
+
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+      : "=&a" (result) : "m" (*uaddr));
+
+  return result;
+}
+
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  ASSERT (udst != NULL);
+
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+      : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+
+  return error_code != -1;
 }
