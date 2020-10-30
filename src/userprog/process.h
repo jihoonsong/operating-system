501fab382ad65aa729457be8bb599e9ddfeb9ccf
Jihoon Song <jihoonsong.dev@gmail.com>
2020-10-30 18:58:30 +0000

    Add elements for user process and PCB
    A process control block is represented as struct process.
    
    Initially, "main" thread creates the first user process. Any user
    process can spwan other user processes.
    
    Note that struct thread can be a kernel thread or a user process. For each user
    process, there is one process control block per one user thread. Thus,
    a tid of a user thread and a pid of process control block of it are the same.
    
    Each thread points to its parent thread. If it is user process, then it also
    poitns to its process control block. A user process that has children points to
    process control blocks of its children.
    
    Resolves: #32

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -1,7 +1,29 @@
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
 
+#include "threads/synch.h"
 #include "threads/thread.h"
+
+/* Process identifier. */
+typedef int pid_t;
+#define PID_ERROR ((pid_t) -1)
+
+/* A process control block. */
+struct process
+  {
+    /* Owned by process.c. */
+    pid_t pid;              /* Process identifier. */
+    bool alive;             /* True if exited, false otherwise. */
+    bool orphan;            /* True if orphan, false otherwise. */
+    bool being_waited;      /* True if wait is called on, false otherwise. */
+    bool start_success;     /* True if started successfully, false otherwise. */
+    int exit_status;        /* Exit status. */
+    struct semaphore start; /* Semaphore to synchronize execution start. */
+    struct semaphore wait;  /* Semaphore to synchronize waiting for child. */
+
+    /* Shared between process.c and thread.c. */
+    struct list_elem elem;  /* List element. */
+  };
 
 tid_t process_execute (const char *task);
 int process_wait (tid_t);
