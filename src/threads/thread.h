2087291fd7dd93488f6ba392d02618986ed4fb9d
Jihoon Song <jihoonsong.dev@gmail.com>
2020-12-05 18:00:05 +0000

    Add MIN and MAX of niceness property
    Resolves: #91, #92

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -39,6 +39,10 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+/* Thread niceness. */
+#define NICE_MIN -20                    /* Lowest niceness. */
+#define NICE_MAX 20                     /* Highest niceness. */
+
 /* A kernel thread or user process.
 
    Each thread structure is stored in its own 4 kB page.  The
