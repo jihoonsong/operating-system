5d143993845d82092f3f62d54b1241b6a6d20ad8
Jihoon Song <jihoonsong.dev@gmail.com>
2020-12-06 01:02:32 +0000

    Increment recent_cpu per tick
    Resolves: #91, #93

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -214,6 +214,9 @@ timer_interrupt (struct intr_frame *args UNUSED)
   /* Unblock threads need to wake up. */
   timer_wake_up ();
 
+  /* Update data for BSD scheduler per tick. */
+  thread_increment_recent_cpu ();
+
   /* Update data for BSD scheduler per second. */
   if (timer_ticks () % TIMER_FREQ == 0)
     {
