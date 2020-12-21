7712b636d72c1335b0f78e784963f4926a199243
Jihoon Song <jihoonsong.dev@gmail.com>
2020-12-21 16:28:22 +0000

    Add get frame and free frame functions of frametab
    Resolves: #100

--- src/vm/frametab.c
+++ src/vm/frametab.c
@@ -1,5 +1,7 @@
 #include "vm/frametab.h"
+#include <debug.h>
 #include <list.h>
+#include "threads/palloc.h"
 #include "threads/synch.h"
 
 /* A frame table. Frames are inserted in FIFO manner
@@ -15,4 +17,41 @@ frametab_init (void)
 {
   list_init (&frames);
   lock_init (&frame_lock);
+}
+
+/* Allocate a new frame and return its kernel virtual address.
+   If there is no empty slot in the frame table, one frame will be
+   selected as a victim and be evicted to allocate a new frame. */
+void *
+frametab_get_frame (enum palloc_flags flags, void *upage)
+{
+  if (!lock_held_by_current_thread (&frame_lock))
+    lock_acquire (&frame_lock);
+
+  void *kpage = palloc_get_page (PAL_USER | flags);
+  if (kpage == NULL)
+    {
+      // TODO: Swapping have to occur.
+      PANIC ("frametab_get_frame: out of frames");
+    }
+
+  // TODO: Construct a new frame and add to frames.
+
+  lock_release (&frame_lock);
+
+  return kpage;
+}
+
+/* Remove the frame at KPAGE from the frame table and free it. */
+void
+frametab_free_frame (void *kpage)
+{
+  if (!lock_held_by_current_thread (&frame_lock))
+    lock_acquire (&frame_lock);
+
+  // TODO: Remove the frame at KPAGE from frames.
+
+  palloc_free_page (kpage);
+
+  lock_release (&frame_lock);
 }
--- src/vm/frametab.h
+++ src/vm/frametab.h
@@ -1,6 +1,10 @@
 #ifndef VM_FRAMETAB_H
 #define VM_FRAMETAB_H
 
+#include "threads/palloc.h"
+
 void frametab_init (void);
+void *frametab_get_frame (enum palloc_flags flags, void *upage);
+void frametab_free_frame (void *kpage);
 
 #endif /* vm/frametab.h */
