87f4fc643f06c8bdf64d8c93618f6c4b78096093
Jihoon Song <jihoonsong.dev@gmail.com>
2020-12-21 17:15:04 +0000

    Use frame table allocator in process.c
    Currently, lazy loading for a kernel page is not supported yet. It will
    be supported after supplemental page table is added.
    
    Closes: #100

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -17,6 +17,9 @@
 #include "threads/palloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#ifdef VM
+#include "vm/frametab.h"
+#endif
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
@@ -540,15 +543,24 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
+      // TODO: Lazy load KPAGE when VM is set.
+#ifdef VM
+      uint8_t *kpage = frametab_get_frame (PAL_USER, upage);
+#else
       /* Get a page of memory. */
       uint8_t *kpage = palloc_get_page (PAL_USER);
+#endif
       if (kpage == NULL)
         return false;
 
       /* Load this page. */
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
+#ifdef VM
+          frametab_free_frame (kpage);
+#else
           palloc_free_page (kpage);
+#endif
           return false;
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
@@ -556,7 +568,11 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable))
         {
+#ifdef VM
+          frametab_free_frame (kpage);
+#else
           palloc_free_page (kpage);
+#endif
           return false;
         }
 
@@ -576,14 +592,22 @@ setup_stack (void **esp)
   uint8_t *kpage;
   bool success = false;
 
+#ifdef VM
+  kpage = frametab_get_frame (PAL_USER | PAL_ZERO, PHYS_BASE - PGSIZE);
+#else
   kpage = palloc_get_page (PAL_USER | PAL_ZERO);
+#endif
   if (kpage != NULL)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
         *esp = PHYS_BASE;
       else
+#ifdef VM
+        frametab_free_frame (kpage);
+#else
         palloc_free_page (kpage);
+#endif
     }
   return success;
 }
