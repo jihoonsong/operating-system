95443c71ce2d59f2bfa49e5fc58a206e4d4913f0
Jihoon Song <jihoonsong.dev@gmail.com>
2020-12-21 13:40:39 +0000

    Initialize frame table
    Resolves: #100

--- src/vm/frametab.c
+++ src/vm/frametab.c
@@ -1,1 +1,18 @@
 #include "vm/frametab.h"
+#include <list.h>
+#include "threads/synch.h"
+
+/* A frame table. Frames are inserted in FIFO manner
+   for the clock page replacement algorithm. */
+static struct list frames;
+
+/* A frame lock. */
+static struct lock frame_lock;
+
+/* Initialize the frame table and the frame lock. */
+void
+frametab_init (void)
+{
+  list_init (&frames);
+  lock_init (&frame_lock);
+}
--- src/vm/frametab.h
+++ src/vm/frametab.h
@@ -1,5 +1,6 @@
 #ifndef VM_FRAMETAB_H
 #define VM_FRAMETAB_H
 
+void frametab_init (void);
 
 #endif /* vm/frametab.h */
