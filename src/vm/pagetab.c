63e5052352d2c96267f3583e07d66e010dab56cb
Jihoon Song <jihoonsong.dev@gmail.com>
2020-12-22 07:07:13 +0000

    Add supplemental page table at pagetab.c
    Supplemental page table is implemented with a hash table.
    
    Resolves: #101

--- src/vm/pagetab.c
+++ src/vm/pagetab.c
@@ -1,1 +1,37 @@
 #include "vm/pagetab.h"
+#include <hash.h>
+
+/* A supplemental page table. */
+struct pagetab
+  {
+    struct hash pages;
+  };
+
+static bool less_func (const struct hash_elem *a,
+                       const struct hash_elem *b,
+                       void *aux UNUSED);
+static unsigned hash_func (const struct hash_elem *e, void *aux UNUSED);
+
+/* Compares the value of two list elements A and B, given
+   auxiliary data AUX.  Returns true if A is less than B, or
+   false if A is greater than or equal to B. */
+static bool
+less_func (const struct hash_elem *a,
+           const struct hash_elem *b,
+           void *aux UNUSED)
+{
+  struct page *page_a = hash_entry (a, struct page, elem);
+  struct page *page_b = hash_entry (b, struct page, elem);
+
+  return page_a->upage < page_b->upage;
+}
+
+/* Computes and returns the hash value for hash element E, given
+   auxiliary data AUX. */
+static unsigned
+hash_func (const struct hash_elem *e, void *aux UNUSED)
+{
+  struct page *page = hash_entry (e, struct page, elem);
+
+  return hash_int ((int) page->upage);
+}
